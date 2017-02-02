ab0e24f47c410fcd35938ec452f4b89b85929fe0
mto1 <toledanom@hotmail.com>
2017-02-02 18:39:53 +0100

    Work 4 Final

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@
 var DRIVY = DRIVY || {};
 
 (function starter () {
-  var render = function render (actors) {
+	var render = function render (actors) {
     var template = document.querySelector('#template').innerHTML;
 
     Mustache.parse(template);   // optional, speeds up future uses
@@ -22,11 +22,47 @@ var DRIVY = DRIVY || {};
     var end = document.querySelector('.end').value;
     var distance = document.querySelector('.distance').value;
     var option = document.querySelector('.option').checked;
-
+	
+	document.querySelector('#car1 .debut').value = document.querySelector('.begin').value;
+	document.querySelector('#car1 .fin').value = document.querySelector('.end').value;
+	document.querySelector('#car1 .distance1').value = document.querySelector('#car .distance').value
+	
+	var begin1 = new Date(begin).getTime();
+    var end1 = new Date(end).getTime();
+	var nbdejour = Math.floor(((end1 - begin1) / (1000 * 60 * 60 * 24)) + 1);
+    document.querySelector('#car1 .jour').value = nbdejour +"j";
+	
+	//document.querySelector('#car1 .total_jour').value = nbdejour * document.querySelector('#car1 .price-by-day1').value;
+	//document.querySelector('#car1 .total_km').value =  document.querySelector('#car .distance').value * document.querySelector('#car1 .price-by-km1').value; 
+	
+	
     var actors = DRIVY.payActors(car, begin, end, distance, option);
-
-    render(actors);
-
+    //render(actors);
+	
+	
+	for(var i =0; i<actors.length; i++)
+	{
+		if(actors[i].who == 'owner')
+		{
+			document.querySelector('#car1 .owner').value = actors[i].amount;		
+		}
+		else if(actors[i].who == 'insurance')
+		{
+			document.querySelector('#car1 .insurance').value = actors[i].amount;
+		}
+		else if(actors[i].who == 'assistance')
+		{
+			document.querySelector('#car1 .assistance').value = actors[i].amount;
+		}
+		else if(actors[i].who == 'drivy')
+		{
+			document.querySelector('#car1 .drivy').value = actors[i].amount;
+		}
+		else if(actors[i].who == 'driver')
+		{
+			document.querySelector('#car1 .driver').value = actors[i].amount;
+		}
+	}
     return;
   });
 }());
